e8bec846d7b1854d99a29211d5cc37faa6aaef3b
Krishna47ak <anandakrishna720@gmail.com>
2024-12-02 11:41:21 +0530

    [ updates & improvements in backend ]

--- server/server.js
+++ server/server.js
@@ -1,18 +1,21 @@
 import express from "express";
 import { dbConnect } from "./data/dbConnect.js";
 import characterRoute from "./routes/characterController.js"
-import stepCountRoute from "./routes/stepCount.js"
+import userRoute from "./routes/user.js"
+import cors from 'cors'
 const app = express();
-const port = 3000;
+const port = 8000;
 
 //dbConnect
 dbConnect();
 
 // Middleware to parse JSON requests
 app.use(express.json());
+app.use(cors());
+
 
 app.use("/api/v1/character", characterRoute);
-app.use("/api/v1/step", stepCountRoute);
+app.use("/api/v1/user", userRoute);
 
 // Serve a welcome message at the root
 app.get("/", (req, res) => {
@@ -25,6 +28,11 @@ app.use((req, res, next) => {
 });
 
 // Start the server
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Server is running on http://0.0.0.0:${port}`);
+// app.listen(port, '0.0.0.0', () => {
+//   console.log(`Server is running on http://0.0.0.0:${port}`);
+// });
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
