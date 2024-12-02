97ae05f8055ea3a12b473fdf0ffae437a7e5bb10
Krishna47ak <anandakrishna720@gmail.com>
2024-12-02 10:26:22 +0530

    [ updates & improvements in mobile app ]

--- Mobile/src/components/DetailsCard.js
+++ Mobile/src/components/DetailsCard.js
@@ -5,8 +5,8 @@ import { Button, StyleSheet, Text, View } from 'react-native';
 const DetailsCard = ({ title, details }) => {
     return (
         <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }} >
-            <Text style={{ fontSize: 17, fontWeight: '600', minWidth: 240, marginBottom: 15 }} >{title}</Text>
-            <Text style={{ fontSize: 17, fontWeight: '600', width: 70, marginBottom: 10, color: 'white', textAlign: 'center', paddingVertical: 2, backgroundColor: 'black', borderRadius: 5 }}  >{details}</Text>
+            <Text style={{ fontSize: 17, fontWeight: '600', minWidth: 240, maxHeight: 30, marginBottom: 15 }} >{title}</Text>
+            <Text style={{ fontSize: 17, fontWeight: '600', width: 70, height: 30, marginBottom: 10, color: 'white', textAlign: 'center', paddingVertical: 2, backgroundColor: 'black', borderRadius: 5 }}  >{details}</Text>
         </View>
     )
 }
